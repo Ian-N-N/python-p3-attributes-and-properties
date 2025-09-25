466cecebb726ba2d770eaa0cf4ec873c3ae9ea6f
Ian Njuguna <ian.njuguna@student.moringaschool.com>
2025-09-25 17:54:42 +0300

    attributes and properties

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,34 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Unknown", breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    # name property
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = "Unknown"
+
+    # breed property
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        elif value == "Mutt":  # default fallback, no print
+            self._breed = "Mutt"
+        else:
+            print("Breed must be in list of approved breeds.")
+            self._breed = "Mutt"
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,35 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Unknown", job="Unemployed"):
+        self.name = name
+        self.job = job
+
+    # name property
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            # store in title case
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = "Unknown"
+
+    # job property
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        elif value == "Unemployed":  # default fallback, no print
+            self._job = "Unemployed"
+        else:
+            print("Job must be in list of approved jobs.")
+            self._job = "Unemployed"
